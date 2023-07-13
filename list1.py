3475d63208ec39e162158bb474f45f25363daa06
USHNISH SARKAR <117502402+UshnishRivu14@users.noreply.github.com>
2023-07-13 14:54:39 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,15 @@
+details = ["Ushnish Sarkar",20,"FYBscITAI",9.8]
+print(details)
+print(details[0])
+print(details[1])
+print(details[2])
+print(details[3])
+print(details[-1])
+print(details[-2])
+print(details[-3])
+print(details[-4])
+
+#changing negative indexinto positive index
+
+print(details[-3])
+print(details[len(details)-3])
